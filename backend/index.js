675b0a9463fd5c29a253bf8091883e94bfe16b7b
huuhung <nghuuhung08102002@gmal.com>
2025-07-13 11:33:04 +0700

    firtbackend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import pool from './db.js'
+import pool from './db.js';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -11,19 +13,20 @@ app.use(express.urlencoded({extended:true})); // Parse form dữ liệu từ bod
 app.use(cookieParser()); // Parse cookie từ header request
 
 const createUser = async () => {
-console.log('毎日ITを勉強してるでもがんばってくださいね')
+console.log('毎日ITを勉強してるでもがんばって くださいね')
 console.log('Test commit 2')
 console.log('Test commit 3')
 };
-const corsOptions = {
-    origin: 'http://localhost:5173',
+const corsOptions = { 
+    origin: 'http://localhost:5173',  
     credentials:true
 }
-
+  
 app.use(cors(corsOptions));
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
-  await createUser();
+  // await createUser();
+  console.log(`Server run PORT ${PORT}`)
 })
 
